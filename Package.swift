10fbba86ff05975b3f16e68a5089765a467fbc18
Aashish <aashish.saini@mediologysoftware.com>
2026-02-27 14:29:29 +0530

    first commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "EpaperSDK",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "EpaperSDK",
+            targets: ["EpaperSdk"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "EpaperSdk",
+            url: "",
+            checksum: ""
+        )
+    ]
+)
+
